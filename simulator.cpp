e57fc0049a4a91bc97cf655dedee60314675097c
ibraaahim11 <ibrahimhesham21@gmail.com>
2024-12-20 16:46:20 +0200

    Fixed input files

--- simulator.cpp
+++ simulator.cpp
@@ -12,4 +12,14 @@ int main() {
 
 	Organizer Org;
 	Org.Simulate("small_input_test_2.txt");
+
+		// sample_input.txt
+		// sample_input_2.txt
+		// sample_input_3.txt
+		// sample_input_4.txt
+		// sample_input_5.txt
+		// sample_input_6.txt
+
+		// small_input_test_2.txt
+		// small_test_file.txt
 }
